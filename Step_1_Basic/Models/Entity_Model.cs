a369f2d04db69c647e32bed561a1cd6023a2f122
kipikipoddev <kipi.kipod.dev@gmail.com>
2024-06-04 15:05:09 +0300

    Step_1

---
+++ Step_1_Basic/Models/Entity_Model.cs
@@ -0,0 +1,10 @@
+
+public class Entity_Model
+{
+    public int Hp { get; set; }
+
+    public Entity_Model(int hp)
+    {
+        Hp = hp;
+    }
+}
---
+++ Step_1_Basic/Scenes/Entity/Entity.cs
@@ -0,0 +1,28 @@
+using Godot;
+
+public partial class Entity : Node
+{
+    [Export]
+    public Entity Enemy;
+
+    private Label hp_lable;
+    protected Entity_Model Model;
+
+    public override void _Ready()
+    {
+        hp_lable = GetNode<Label>("Hp_Label");
+        Model = new Entity_Model(5);
+        Update();
+    }
+
+    public void Update()
+    {
+        hp_lable.Text = Model.Hp.ToString("D2");
+    }
+
+    public void On_button_pressed()
+    {
+        Enemy.Model.Hp--;
+        Enemy.Update();
+    }
+}
